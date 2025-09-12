c51cc56855d241df8e5fb1142fe02b50ea070058
danst3in <50937807+danst3in@users.noreply.github.com>
2025-09-12 12:06:29 -0700

    feat: implement binary search function in bs_list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let low = 0;
+    let high = haystack.length;
+    // let mid = Math.floor(low + (low+high)/2)
+    while (low < high) {
+        const mid = Math.floor(low + (high - low) / 2);
+        if (haystack[mid] === needle) {
+            return true;
+        } else if (haystack[mid] < needle) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+    return false;
 }
