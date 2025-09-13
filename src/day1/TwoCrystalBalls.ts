95aa0147cd918970ea18ff77e2655d66b54c8e2f
danst3in <50937807+danst3in@users.noreply.github.com>
2025-09-13 11:07:18 -0700

    feat: add initial implementation of two crystal balls algorithm

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let low = 0;
+    let jump = Math.floor(Math.sqrt(breaks.length));
+    let i = jump;
 
+    // find where crystal ball is broken
+
+    for (; i < breaks.length; i += jump)
+        if (breaks[i]) {
+            break;
+        }
+
+    // back up one jump and walk the length of a jump to find the exact spot where it breaks
+    i -= jump;
+
+    for (let j = 0; j < jump && i < breaks.length; j++, i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
