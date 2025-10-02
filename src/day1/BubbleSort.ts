215f85366b7caec3c9ade475b88d90a068d9758f
danst3in <50937807+danst3in@users.noreply.github.com>
2025-10-01 17:51:33 -0700

    feat: implement bubble sort algorithm

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
-
+    // cycle through the entire array once up to but not including final value because final value n+1 doesn't exist for comparison
+    for (let i = 0; i < arr.length; i++) {
+        // shrink the array to search by 1 each time since the most recent final value will be sorted in previous iteration
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
