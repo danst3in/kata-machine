752864eb3c63828e031da08adbaf550a50958956
danst3in <50937807+danst3in@users.noreply.github.com>
2025-10-04 14:46:06 -0700

    fix: implement constructor and peek method in Queue class

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -7,9 +7,14 @@ export default class Queue<T> {
     private head?: Node<T>;
     private tail?: Node<T>;
 
-    constructor() {}
+    constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
+    }
 
     enqueue(item: T): void {}
     deque(): T | undefined {}
-    peek(): T | undefined {}
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
