863f496ecdf0a3a0daf093e80b6543bd7938c425
danst3in <50937807+danst3in@users.noreply.github.com>
2025-10-05 16:48:48 -0700

    feat: implement algorithm partitioning logic

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,22 @@
-export default function quick_sort(arr: number[]): void {
+const qs = (arr: number[], lo: number, hi: number): void => {};
+const partition = (arr: number[], lo: number, hi: number): number => {
+    const pivot: number = arr[hi];
+    let idx = lo - 1;
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+    idx++;
 
+    arr[hi] = arr[idx];
+    arr[idx] = arr[pivot];
+
+    return idx;
+};
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
