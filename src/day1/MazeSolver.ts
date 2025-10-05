afe04a7c4f38dad6adba30f17bca579ccec4f5b5
danst3in <50937807+danst3in@users.noreply.github.com>
2025-10-05 16:07:54 -0700

    feat: enhance walk function for maze traversal with improved path handling

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,9 @@
+const dir = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+];
 function walk(
     maze: string[],
     wall: string,
@@ -23,6 +29,7 @@ function walk(
     }
     // 3. reached the end of maze
     if (curr.x === end.x && curr.y === end.y) {
+        path.push(end);
         return true;
     }
     // 4. previously visited
@@ -32,9 +39,22 @@ function walk(
 
     // Recurse
     // pre
+    seen[curr.y][curr.x] = true;
+    path.push(curr);
 
     // start recurse
+    for (let i = 0; i < dir.length; i++) {
+        const [x, y] = dir[i];
+        if (
+            walk(maze, wall, { x: curr.x + x, y: curr.y + y }, end, seen, path)
+        ) {
+            return true;
+        }
+    }
+
     // post
+    path.pop();
+    return false;
 }
 export default function solve(
     maze: string[],
@@ -47,4 +67,14 @@ export default function solve(
     // 2. off the map
     // 3. previously visited
     // 4. reached the end of maze
+
+    const seen: boolean[][] = [];
+    const path: Point[] = [];
+
+    // manually set all seen[] values to false
+    for (let i = 0; i < maze.length; i++) {
+        seen.push(new Array(maze[0].length).fill(false));
+    }
+    walk(maze, wall, start, end, seen, path);
+    return path;
 }
