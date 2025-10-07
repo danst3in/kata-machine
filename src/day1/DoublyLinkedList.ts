d334dcf769c1d9f49e7b701b588fbd65a84b349b
danst3in <50937807+danst3in@users.noreply.github.com>
2025-10-06 18:38:41 -0700

    feat: implement insertAt method for DoublyLinkedList and improve prepend logic

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -6,25 +6,63 @@ type Node<T> = {
 export default class DoublyLinkedList<T> {
     public length: number;
     private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
         this.length = 0;
         this.head = undefined;
+        this.tail = undefined;
     }
 
     prepend(item: T): void {
         const node = { value: item } as Node<T>;
         this.length++;
         if (!this.head) {
-            this.head = node;
+            this.head = this.tail = node;
             return;
         }
         node.next = this.head;
         this.head.prev = node;
         this.head = node;
+        return;
     }
-    insertAt(item: T, idx: number): void {}
-    append(item: T): void {}
+    insertAt(item: T, idx: number): void {
+        if (idx > this.length) {
+            throw new Error("index too great. you don't have the right.");
+        } else if (idx === this.length) {
+            this.append(item);
+            return;
+        } else if (idx === 0) {
+            this.prepend(item);
+            return;
+        }
+        this.length++;
+        const node = { value: item } as Node<T>;
+        let curr = this.head;
+        for (let i = 0; curr && i < idx; i++) {
+            curr = curr.next;
+        }
+        curr = curr as Node<T>;
+        node.next = curr;
+        node.prev = curr.prev;
+        if (curr.prev) {
+            curr.prev.next = node;
+        }
+        curr.next = curr?.next?.next;
+        return;
+    }
+    append(item: T): void {
+        this.length++;
+        const node = { value: item } as Node<T>;
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
+        node.prev = this.tail;
+        this.tail.next = node;
+        this.tail = node;
+        return;
+    }
     remove(item: T): T | undefined {}
     get(idx: number): T | undefined {
         let current = this.head;
