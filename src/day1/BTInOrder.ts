e00699735d2507d8400b53231316996110933a02
danst3in <50937807+danst3in@users.noreply.github.com>
2025-10-06 21:40:17 -0700

    Implement BT in order traversal with `walk` helper function

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,20 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number | undefined>, path: number[]): number[] {
+    if (curr.left) {
+        walk(curr.left, path);
+    }
+    if (!curr) {
+        return path;
+    } else if (curr.value) {
+        path.push(curr.value);
+    }
 
+    if (curr.right) {
+        walk(curr.right, path);
+    }
+
+    return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
