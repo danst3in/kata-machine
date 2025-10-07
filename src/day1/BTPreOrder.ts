ac3644b6954c626972f5a474c4f72a5955f63076
danst3in <50937807+danst3in@users.noreply.github.com>
2025-10-06 21:38:29 -0700

    implement pre_order_search BT traversal to use a helper walk function for clarity

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,20 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number | undefined>, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    } else if (curr.value) {
+        path.push(curr.value);
+    }
 
+    if (curr.left) {
+        walk(curr.left, path);
+    }
+
+    if (curr.right) {
+        walk(curr.right, path);
+    }
+
+    return path;
+}
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
