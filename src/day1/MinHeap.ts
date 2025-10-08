946edc09396cdf73e0f69e19e1c30a77de3ade53
danst3in <50937807+danst3in@users.noreply.github.com>
2025-10-08 10:15:26 -0700

    implement minHeap constructor + private methods to identify parent left + right child

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,23 @@
 export default class MinHeap {
     public length: number;
-
-    
+    private data: number[];
 
     constructor() {
+        this.data = [];
+        this.length = 0;
     }
 
-    insert(value: number): void {
+    insert(value: number): void {}
+    delete(): number {}
 
-}
-    delete(): number {
+    private parent(idx: number): number {
+        return Math.floor((idx - 1) / 2);
+    }
 
-}
+    private leftChild(idx: number): number {
+        return idx * 2 + 1;
+    }
+    private rightChild(idx: number): number {
+        return idx * 2 + 2;
+    }
 }
