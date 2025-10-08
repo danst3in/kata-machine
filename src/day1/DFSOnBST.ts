d8e5368a6844358bad438dae447f829c73bc5c9b
danst3in <50937807+danst3in@users.noreply.github.com>
2025-10-07 18:25:34 -0700

    test dfsonbst without 2nd value comparison

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -8,7 +8,7 @@ export default function dfs(head: BinaryNode<number>, needle: number): boolean {
     if (head.value > needle && head.left) {
         return dfs(head.left, needle);
     }
-    if (head.value < needle && head.right) {
+    if (/* head.value < needle && */ head.right) {
         return dfs(head.right, needle);
     }
     return false;
