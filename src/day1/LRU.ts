e1c26328094c0202a5c36ad4c785cc69ad57d9a4
danst3in <50937807+danst3in@users.noreply.github.com>
2025-10-09 14:41:27 -0700

    add pseudocode to lru

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,15 +1,17 @@
 export default class LRU<K, V> {
     private length: number;
 
-    
-
-    constructor() {
-    }
+    constructor() {}
 
     update(key: K, value: V): void {
-
-}
+        // does it exist?
+        // if not we need to insert
+        // check capacity and evict if over capacity
+        // if it does we need to update to front of list and update the value
+    }
     get(key: K): V | undefined {
-
-}
+        //  check the cache for existence
+        //  update value found by moving it to the head
+        //  return out value found or undefined if doesn't exist
+    }
 }
