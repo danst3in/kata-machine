d4c779a68dbb57a44d57627113d647649363d996
danst3in <50937807+danst3in@users.noreply.github.com>
2025-10-09 14:56:04 -0700

    create node type, private methods and properties, and constructor for LRU

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,7 +1,26 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+};
+
+function createNode<V>(value: V): Node<V> {
+    return { value };
+}
 export default class LRU<K, V> {
     private length: number;
+    private head?: Node<V>;
+    private tail?: Node<V>;
 
-    constructor() {}
+    private lookup: Map<K, Node<V>>;
+    private reverseLookup: Map<Node<V>, K>;
+
+    constructor(private capacity: number = 10) {
+        this.length = 0;
+        this.head = this.tail = undefined;
+        this.lookup = new Map<K, Node<V>>();
+        this.reverseLookup = new Map<Node<V>, K>();
+    }
 
     update(key: K, value: V): void {
         // does it exist?
