9750447daa49a0b5563eba05652ce9e3b058ea66
danst3in <50937807+danst3in@users.noreply.github.com>
2025-10-09 15:43:36 -0700

    implement private methods detach, prepend, trimCache for LRU

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -30,14 +30,17 @@ export default class LRU<K, V> {
             node = createNode(value);
             this.length++;
             this.prepend(node);
+            // check capacity and evict if over capacity
             this.trimCache();
+
+            this.lookup.set(key, node);
+            this.reverseLookup.set(node, key);
         } else {
             // if it does we need to update to front of list and update the value
             this.detach(node);
-            // node = createNode(value)
             this.prepend(node);
+            node.value = value;
         }
-        // check capacity and evict if over capacity
     }
     get(key: K): V | undefined {
         //  check the cache for existence
@@ -53,7 +56,44 @@ export default class LRU<K, V> {
         return node.value;
     }
 
-    private detach(node: Node<V>) {}
-    private prepend(node: Node<V>) {}
-    private trimCache(): void {}
+    private detach(node: Node<V>) {
+        if (node.prev) {
+            node.prev.next = node.next;
+        }
+        if (node.next) {
+            node.next.prev = node.prev;
+        }
+
+        if (this.head === node) {
+            this.head = this.head.next;
+        }
+        if (this.tail === node) {
+            this.tail = this.tail.prev;
+        }
+        node.next = undefined;
+        node.prev = undefined;
+    }
+    private prepend(node: Node<V>) {
+        if (!this.head) {
+            this.head = this.tail = node;
+            return;
+        }
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
+    private trimCache(): void {
+        if (this.length <= this.capacity) {
+            return;
+        }
+        const tail = this.tail!;
+        this.detach(this.tail!);
+
+        // remove the tail from map as well as dbl linked list
+
+        const key = this.reverseLookup.get(tail);
+        this.lookup.delete(key!);
+        this.reverseLookup.delete(tail);
+        this.length--;
+    }
 }
